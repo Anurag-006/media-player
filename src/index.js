27f55b87d09b42b68c7e069d3c8e28957b465f91
Anurag Kosuri <135948024+Anurag-006@users.noreply.github.com>
2024-12-30 03:35:21 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
-// const app = express();
+const app = express();
 
 dotenv.config({
     path: "./env"
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ SERVER is running at PORT: ${process.env.PORT}`);
+    })
+})
+.catch((err) => console.log("MONOGODB connection failed: ",err))
 
 // ( async () => {
 //     try {
