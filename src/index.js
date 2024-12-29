c8be9cf24aa65b3c2f76310bcaa4408d5c842ad2
Anurag Kosuri <135948024+Anurag-006@users.noreply.github.com>
2024-12-29 19:32:39 +0530

    setup mongodb

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+// const app = express();
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB();
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`process is running on PORT: ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+// })()
